79b4c4aff3b1b0a451ac60c8f0bfe152d59c5a18
Martin <MartinBechtle@users.noreply.github.com>
2024-09-12 18:15:56 +0200

    fix dockerfile

--- .nitro/benchmark/run.sh
+++ .nitro/benchmark/run.sh
@@ -4,11 +4,12 @@ set -e
 echo "Started enclave"
 pwd
 ls -al
-cd /app/leaf
+cd /app/leaf # necessary to use absolute path as the nitro enclave runs processes from the root, unlike `docker run` which runs from the workdir
 go version
 
 echo "Starting benchmarks"
 echo "Home: $HOME"
+export GOTMPDIR=/app/tmp
 go test -bench=. -cpu=1
 
 # go in a sleep loop us to peek into the nitro console
